d98d1116af471d5357d92d71f1a9db22a00fb9f9
Ralf Asmus <info@ralfasmus.de>
2018-09-29 07:15:58 +0200

    work version auf priv ausgerollt

---
+++ www/memo/priv/privpass-dev/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// Verzeichnis, in dem www, php u.a. liegt
+define("APPLICATION_ROOT",dirname(realpath("."),4));
+define("APPLICATION_NAME",basename(realpath(".")));
+
+require_once("local.version.inc.php");
+require_once("../../default/index.inc.php");
