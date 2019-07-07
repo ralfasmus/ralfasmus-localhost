d0f6924ee5b331b45c97a2392079fadce9ba3b18
Ralf Asmus <info@ralfasmus.de>
2019-07-07 04:54:44 +0200

    ok

---
+++ www/memo/default/index.inc.php
@@ -0,0 +1,45 @@
+<?php
+
+$vhost = $_SERVER['HTTP_HOST'];
+$vhost = strpos($vhost, ":") === false ? $vhost : substr($vhost, 0, strpos($vhost, ":"));
+define('VHOST_DOMAIN', $vhost);
+
+define('PHP_DIR', APPLICATION_ROOT . "/php/memo/" . APPLICATION_VERSION_PHP);
+//error_reporting(E_ALL);
+error_reporting(E_ALL | E_STRICT);
+setlocale(LC_TIME, 'de_DE.utf8'); // Locale muss auf dem Server installiert sein (vbox!). Fuer Ausgabe Wochentag
+ini_set("display_startup_errors", "On");
+ini_set("display_errors", "On");
+// um ein Jahr ARB Daten zu verarbeiten, wird ein hoeheres memory limit benoetigt:
+ini_set('memory_limit', '4134217728');
+spl_autoload_register(function($class) {
+  $className = $class;
+  $className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
+  if (strpos($class, "//") !== FALSE) {
+    list($fae, $className) = explode("//", $class, 2);
+  } else if (strpos($class, "\\") !== FALSE) {
+    list($fae, $className) = explode("\\", $class, 2);
+  }
+  include PHP_DIR . "/$className.php";
+});
+$phpProperties = array();
+// Die Reihenfolge ist entscheidend!
+// wer zuerst steht, ueberschreibt andere, wenn der
+// request nach einer property gefragt wird.
+foreach (array(
+"server" => $_SERVER,
+ "get" => $_GET,
+ "post" => $_POST,
+ "session" => &$_SESSION,
+ "cookie" => &$_COOKIE,
+ "files" => &$_FILES,
+ "request" => &$_REQUEST,
+) as $arrayName => $arr) {
+  if (is_array($arr)) {
+    $phpProperties[$arrayName] = $arr;
+  }
+}
+
+$request = Request::getSingleInstance($phpProperties);
+Zeit::setRequestStartzeit();
+echo View::create()->getResponse();
